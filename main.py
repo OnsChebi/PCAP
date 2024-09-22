eeda13ff9288ad7f9b65acb600bbacfa6b009e23
OnsChebi <chebbions7@gmail.com>
2024-09-22 21:33:35 +0100

    woring with strings

---
+++ main.py
@@ -0,0 +1,24 @@
+# from module import sum
+# # import test
+# #print(__name__)
+# #print(module.counter)
+# la_list=[i+1 for i in range (5)]
+# print("sum= ", sum(la_list))
+# import sys
+# for p in sys.path:
+#     print(p)
+ 
+# from  sys import path
+# path.append('.\\modules')
+
+# import module
+
+
+
+# greeting="hello world!"
+# for i in range(len(greeting)):
+#     print(greeting[i],end=' **')
+    
+greeting="hello world!"
+for i in greeting:
+    print(i,end=' **')
---
+++ modules/module.py
@@ -0,0 +1,41 @@
+# #counter=0
+
+
+# # if __name__=="__main__":
+# #     print("hello world!")
+# #     print (counter)
+
+
+# def sum (list):
+#     sum=0
+#     for element in list:
+#         sum+=element
+#     return sum
+
+# def prod (list):
+#     prod=1
+#     for element in list:
+#         prod *=element
+#     return prod 
+    
+# my_list=[i+1 for i in range (10)]
+
+
+# # ken nhb ay 7aja trunni 3l file hedha brk 
+# if __name__== "__main__" :
+#     print("sum= ", sum(my_list))
+#     print("prod= ",prod(my_list))
+#     print (prod == 15)
+# #print(my_list)
+
+
+
+import sys
+
+
+if  __name__ != "__main__":
+    print("you can't run it ")
+    sys.exit()
+else :
+    print("hello world")
+# # print(__name__)
